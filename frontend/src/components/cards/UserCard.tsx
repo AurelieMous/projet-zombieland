1561b31ea8e093f1619d1b3f971fa21ba85a4625
Aurélie <naoorelie@gmail.com>
2025-12-11 10:51:42 +0100

    add modal to update profil

--- frontend/src/components/cards/UserCard.tsx
+++ frontend/src/components/cards/UserCard.tsx
@@ -2,12 +2,18 @@ import {Box, Card, CardActions, CardContent, Typography} from "@mui/material";
 import type {User} from "../../@types/users";
 import {formatDay} from "../../functions/formatDay.ts";
 import {PrimaryButton} from "../common";
+import {useState} from "react";
+import UpdateProfilModal from "../modals/UpdateProfilModal.tsx";
 
 interface UserCardProps {
     user: User
 }
 
 export default function UserCard({user} : UserCardProps) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <Card sx={{ minWidth: 450 }}>
             <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: 2, alignItems: 'center' }}>
@@ -27,9 +33,11 @@ export default function UserCard({user} : UserCardProps) {
 
             </CardContent>
             <CardActions sx={{display: 'flex', flexDirection: 'column', gap: 5}}>
-                <PrimaryButton text={"Modifier l'email"} />
-                <PrimaryButton text={"Modifier le mot de passe"} />
+                <PrimaryButton text={"Modifier l'email"} onClick={handleOpen}/>
+                <PrimaryButton text={"Modifier le mot de passe"} onClick={handleOpen}/>
             </CardActions>
+            <UpdateProfilModal open={open}  onClose={handleClose}/>
         </Card>
+
     );
 }
