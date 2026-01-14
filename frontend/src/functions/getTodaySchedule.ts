fa4cec72661119fdb6485ff0f1f197bcdf2bcf5f
Aurélie <naoorelie@gmail.com>
2026-01-14 11:22:53 +0100

    update image banner styles and fix import schedule data type

--- frontend/src/functions/getTodaySchedule.ts
+++ frontend/src/functions/getTodaySchedule.ts
@@ -37,7 +37,15 @@ const getTodaySchedule = async () => {
     } catch (error) {
         // En cas d'erreur, afficher un message d'erreur dans la console'
         console.error("Erreur lors de la récupération des horaires:", error);
-        return null
+        return {
+            id: 0,
+                jour: todayString,
+                open_hour: "",
+                close_hour: "",
+                is_open: false,
+                notes: null,
+                created_at: new Date().toISOString()
+        };
     }
 };
 
